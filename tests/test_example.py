ceed89b172fe37c4cbe18ae7ebf234a0de844375
Gowtham Rao <gowthamrao@gmail.com>
2025-10-25 19:14:33 -0400

    feat: Apply Prosperity Public License (#15)
    This commit applies the Prosperity Public License to the project.
    
    - Adds a `NOTICE` file with the required information.
    - Adds the license header to all Python source files.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,2 +1,13 @@
+# Copyright (c) 2025-2026 CoReason, Inc. All Rights Reserved.
+#
+# This software is proprietary and dual-licensed.
+# Licensed under the Prosperity Public License 3.0.0 (the "License").
+# A copy of the license is available at https://prosperitylicense.com/versions/3.0.0
+# For details, see the LICENSE file.
+#
+# Commercial use beyond a 30-day trial requires a separate license.
+# Contact: gowtham.rao@coreason.ai
+
+
 def test_always_passes():
     assert True
